83d95e17634aaabad19d948bcc97e4e212a12a59
tnfsp <aa2670095@gmail.com>
2025-12-04 09:53:13 +0000

    Update workflow and dependencies

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     'ga': 'G-6C4Z9NHW8J',
-    'whiteboard_id': 'd4cc3728297609add1a00aab108e90c4e57a1c378cfc2307c251745bf7d2a884',
+    'whiteboard_id': '713dfe51-f65b-4105-98fe-8d2278170bd9',
     'title': '数字花园🌱', // 站点标题
     'pages': {
-        'Articles': '9f31ea21-90b9-4523-b8d5-cb33b7a01bda',
-        'Projects': '3dd9a603-a7f3-44e9-a6d7-cd2ebda08952',
+        'Articles': 'f78da9f7-8aa7-4e7c-9558-31c236f855fe',
+        'Projects': '06f62683-9815-4871-b046-7085cb8ad676',
         // 'Activity':'activity', // 花园活跃状态页面
-        'About': '3a433c0b-e2e1-4722-8a88-a17e9aa2b927'
+        'About': '3b3e0536-aeb6-4288-97b2-c04bdda81d8a'
     },
     'server': '1214087861653995540', // Discord 服务器 ID，非必填，填写后将在网站中显示聊天入口
     'channel': '1214087861653995545' // Discord 频道 ID，非必填，填写后将在网站中显示聊天入口
